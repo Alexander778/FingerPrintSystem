ec035f2960e83d5ad5025c8c615724850e15f370
Alexander <Alexander@WIN-P7V0JSUPBKU>
2017-09-20 21:01:01 +0300

    Исправил (Week3_Task20)согласно требований от 20.09.17

---
+++ Week3_Task20/Week3_Task20/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+
+
+namespace Week3_Task20
+{
+    class Program
+    {
+
+
+        
+        static void Main(string[] args)
+        {
+            {
+                bool ch = true;
+                while (ch == true)
+                {
+                    Console.WriteLine("Computer Locker");
+                    Console.WriteLine("");
+                    Console.WriteLine("Do you want to lock your computer?Y/N");
+                    string decision = Console.ReadLine();
+                    string d = decision.ToLower();
+                    if (d == "y" || d == "n")
+                    {
+                        switch (d)
+                        {
+                            case "y":
+                                Win32.LockWorkStation();
+                                break;
+                            case "n":
+                                Environment.Exit(0);
+                                break;
+                            default:
+                                break;
+                        }
+
+                    }
+
+                    else
+                    {
+                        ch = false;
+                        Console.WriteLine("Your information is wrong. Use only y/n!");
+                        ch = true;
+                    }
+                    
+                }
+
+            }
+                
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Week3_Task20/Week3_Task20/Win32.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Week3_Task20
+{
+     public class Win32
+        {
+            [DllImport("user32.dll")]
+            public static extern void LockWorkStation();
+        }
+    
+}
