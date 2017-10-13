671c73130177ec9d4a8131c2093a63acf747a25c
Alexander <Alexander@ALEXPC>
2017-10-13 20:11:29 +0300

    1

--- FingerPrintDB1/FingerPrintDB1/dbo/Tables/tbo_ConnectPrDe.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[tbo_ConnectPrDe] (
-    [Department_ID] INT NOT NULL,
-    [User_ID]       INT NOT NULL,
-    CONSTRAINT [PK_tbo_Example] PRIMARY KEY CLUSTERED ([User_ID] ASC),
-    CONSTRAINT [FK_tbo_Example_tbo_Department] FOREIGN KEY ([Department_ID]) REFERENCES [dbo].[tbo_Department] ([Department_ID]),
-    CONSTRAINT [FK_tbo_Example_tbo_Profile] FOREIGN KEY ([User_ID]) REFERENCES [dbo].[tbo_Profile] ([User_ID])
-);
-
