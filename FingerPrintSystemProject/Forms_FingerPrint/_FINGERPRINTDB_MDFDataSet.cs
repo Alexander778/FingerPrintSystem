f50a6ac93b8973e6a9b39baf3b5b43d58c5052a0
Alexander <Alexander@ALEXPC>
2017-10-21 17:50:29 +0300

    21.10.17 evening(end with link table) delete/add

---
+++ FingerPrintSystemProject/Forms_FingerPrint/_FINGERPRINTDB_MDFDataSet.cs
@@ -0,0 +1,15 @@
+﻿namespace Forms_FingerPrint
+{
+
+
+    partial class _FINGERPRINTDB_MDFDataSet
+    {
+    }
+}
+
+namespace Forms_FingerPrint._FINGERPRINTDB_MDFDataSetTableAdapters {
+    
+    
+    public partial class tbo_DepartmentTableAdapter {
+    }
+}
